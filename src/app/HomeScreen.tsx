376ce50cda19b1bcc6c431432cd9e10b0672cca3
dishapatel3195 <116899905+dishapatel3195@users.noreply.github.com>
2024-07-10 22:58:51 -0400

    Worked on the HomeScreen

--- src/app/HomeScreen.tsx
+++ src/app/HomeScreen.tsx
@@ -6,10 +6,99 @@ import { db } from '../support/firebase';
 import React from 'react';
 
 const Tab = createMaterialBottomTabNavigator();
+const logoRed = '#EF6466'
+const whiteGrey = '#EFEFEF'
 
 export default function homeScreen() {
     return (
-        <YStack flex={1} justifyContent='center' alignItems='center' > 
-        <Text>HOME</Text>
+        <YStack flex={1}> 
+            <Text  style={{fontFamily: 'Arial Rounded MT bold', top: 80, left: 25}} fontSize={50}  color = "#EF6466" >
+                Hi Student.
+            </Text>
+            <XStack
+                style={{
+                    top: 90,
+                    left: 25,
+                    width: 340,
+                    height: 70,  // Adjust height as needed
+                    backgroundColor: whiteGrey,
+                    padding: 10, 
+                    borderRadius: 10
+                }}
+            >
+            </XStack>
+            <XStack
+                style={{
+                    top: 100,
+                    left: 25,
+                    width: 340,
+                    height: 150,  // Adjust height as needed
+                    backgroundColor: whiteGrey,
+                    padding: 10, 
+                    borderRadius: 10
+                }}
+            >
+                
+                <YStack> 
+                    <Text  style={{fontFamily: 'Arial Rounded MT bold'}} alignContent = "center" fontSize={14}  color = "#2F7B80" lineHeight={35}>
+                        Another Day. Another Slay.
+                    </Text>
+                    <Text  style={{fontFamily: 'Arial Rounded MT bold'}} alignContent = "center" fontSize={14}  color = "#2F7B80" lineHeight={35}>
+                        You Are Exactly Where You Need To Be. You Are Going To Be More Than Okay!
+                    </Text>
+                </YStack>
+            </XStack>
+
+            <Button
+                onPress={() => alert('Goals')}
+
+                style={{
+                    top: 120,
+                    left: 25,
+                    width: 340,
+                    height: 50,  // Adjust height as needed
+                    backgroundColor: whiteGrey,
+                    padding: 10, 
+                    borderRadius: 45
+                }}
+            >
+                <Text  style={{fontFamily: 'Arial Rounded MT bold'}} alignContent = "center" fontSize={20}  color = "#EF6466">
+                    Goals
+                </Text>
+            </Button>
+            <Button
+                onPress={() => alert('WeeklyCalendar')}
+                style={{
+                    top: 140,
+                    left: 25,
+                    width: 340,
+                    height: 50,  // Adjust height as needed
+                    backgroundColor: whiteGrey,
+                    padding: 10, 
+                    borderRadius: 45
+                }}
+            >
+                <Text  style={{fontFamily: 'Arial Rounded MT bold'}} alignContent = "center" fontSize={20}  color = "#EF6466">
+                    Weekly Calendar
+                </Text>
+            </Button>
+            <Button
+                onPress={() => alert('ToDoList')}
+                style={{
+                    top: 160,
+                    left: 25,
+                    width: 340,
+                    height: 50,  // Adjust height as needed
+                    backgroundColor: whiteGrey,
+                    padding: 10, 
+                    borderRadius: 45
+                }}
+            >
+                <Text  style={{fontFamily: 'Arial Rounded MT bold'}} alignContent = "center" fontSize={20}  color = "#EF6466">
+                    To-Do List
+                </Text>
+            </Button>
+
+
         </YStack>  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,52 @@
 import { collection, getDocs } from 'firebase/firestore/lite';
 import { Button, SizableText, Tabs, XStack, YStack, Text } from 'tamagui';
 import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
-import {CircleUserRound, Home, ListTodo, Notebook, Settings, Settings2} from '@tamagui/lucide-icons'
-
+import { CircleUserRound, Home, Notebook, Settings } from '@tamagui/lucide-icons';
 import { db } from '../support/firebase';
 import React from 'react';
 import HomeScreen from './HomeScreen';
 import ClassesScreen from './ClassesScreen';
 import ProfileScreen from './ProfileScreen';
 import SettingsScreen from './SettingsScreen';
-import PlannerScreen from './PlannerScreen';
 
 const Tab = createMaterialBottomTabNavigator();
+const tealBlue = '#2F7B80'
+const darkTealSelection = '#133335'
 
 export default function App() {
-    return (
-      <Tab.Navigator>
-        <Tab.Screen name = "Planner" component={PlannerScreen} options={{tabBarIcon:() => (<ListTodo/>)}}/>
-        <Tab.Screen name="Classes" component={ClassesScreen} options={{tabBarIcon:() => (<Notebook/>)}}/>
-        <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon:() => (<Home/>)}}/>
-        <Tab.Screen name="Profile" component={ProfileScreen} options={{tabBarIcon:() => (<CircleUserRound/>)}}/>
-        <Tab.Screen name="Settings" component={SettingsScreen} options={{tabBarIcon:() => (<Settings/>)}}/>
-      </Tab.Navigator>
+  return (
+      
+    <Tab.Navigator 
+      initialRouteName="Home"
+      activeColor='white'
+      inactiveColor='white'
+      barStyle={{ backgroundColor: tealBlue }} // Set the background color to blue here
+      activeIndicatorStyle={{ backgroundColor: darkTealSelection}}
+    >
+      <Tab.Screen 
+        name="Home" 
+        component={HomeScreen} 
+        options={{ tabBarIcon: ({ color }) => (<Home color={color} />) }} 
+      />
+      <Tab.Screen 
+        name="Classes" 
+        component={ClassesScreen} 
+        options={{ tabBarIcon: ({ color }) => (<Notebook color={color} />) }} 
+      />
+      <Tab.Screen 
+        name="Settings" 
+        component={SettingsScreen} 
+        options={{ tabBarIcon: ({ color }) => (<Settings color={color} />) }} 
+      />
+      <Tab.Screen 
+        name="Profile" 
+        component={ProfileScreen} 
+        options={{ tabBarIcon: ({ color }) => (<CircleUserRound color={color} />) }} 
+      />
+    </Tab.Navigator>
   );
+
 }
+
+
+
